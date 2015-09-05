c01fd95fc0052543c843cdef9b73636b79f26e39
Daniel Lockhart <daniel@lockyy.com>
2015-09-05 02:46:45 +0100

    Don't output file locations

--- lib/placepic.rb
+++ lib/placepic.rb
@@ -1,6 +1,6 @@
 mydir = File.expand_path(File.dirname(__FILE__))
 
-Dir[mydir + '/placepic/*.rb'].each {|file| puts file; require file }
+Dir[mydir + '/placepic/*.rb'].each {|file| require file }
 
 module PlacePic
   ActionView::Base.send :include, PlacePic::Placeholders
