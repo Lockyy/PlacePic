96260afbf955572754af6e4c49815c3b617155cb
Daniel Lockhart <daniel@lockyy.com>
2015-09-11 00:25:39 +0100

    Move functions into class level methods and add instance variables that call them. This way we can properly load them into actionviews and still use them easily in rails c if needs be.

--- lib/placepic/placeholders.rb
+++ lib/placepic/placeholders.rb
@@ -1,17 +1,30 @@
 module PlacePic
   module Placeholders
 
-    def placeholder_tag(width, height, opts = {})
-      image_url = placeholder_url(width, height, opts)
-      tag_opts = opts.except(:style, :site)
-
-      image_tag(image_url, tag_opts)
+    def placeholder_tag(width = 100, height = nil, opts = {})
+      Placeholders.placeholder_tag(width, height, opts)
     end
 
-    def placeholder_url(width, height, opts = {})
-      url, style = PlacePic::Urls.url_and_style(opts)
+    def placeholder_url(width = 100, height = nil, opts = {})
+      Placeholders.placeholder_url(width, height, opts)
+    end
 
-      "//#{url}.com/#{style}/#{width}/#{height}"
+    class << self
+      def placeholder_tag(width = 100, height = nil, opts = {})
+        height = width unless height
+        image_url = placeholder_url(width, height, opts)
+        tag_opts = opts.except(:style, :site)
+        tag_opts[:alt] = "placeholder-#{width}x#{height}" unless tag_opts[:alt]
+
+        ActionController::Base.helpers.image_tag(image_url, tag_opts)
+      end
+
+      def placeholder_url(width = 100, height = nil, opts = {})
+        height = width unless height
+        url, style = PlacePic::Urls.url_and_style(opts)
+
+        "//#{url}.com/#{style}/#{width}/#{height}"
+      end
     end
   end
 end
